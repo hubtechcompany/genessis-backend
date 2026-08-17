fb766e75cc882afee3aa529640b1e588396d9c44
hubtechcompany <habtechcompany@gmail.com>
2026-08-17 15:29:55 +0300

    Update main.py
    rout

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# =========================================================
+# ROUTES
+# =========================================================
+
+app.include_router(conversation_router)
+app.include_router(chat_router)
 
 # =========================================================
 # ROUTES
